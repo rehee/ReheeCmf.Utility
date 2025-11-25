c23d63bdb40243195b012de1fb24ccbd9119a1ba
xh <x3iii131@gmail.com>
2025-11-25 19:26:00 +0000

    更新规则

--- src/ReheeCmf.Utility/Helpers/ContentResponseHelper.cs
+++ src/ReheeCmf.Utility/Helpers/ContentResponseHelper.cs
@@ -1,117 +1,117 @@
+using ReheeCmf.Commons;
 using System;
 using System.Collections.Concurrent;
 using System.Linq;
 using System.Net;
 using System.Reflection;
-using ReheeCmf.Commons;
 
 namespace ReheeCmf.Helpers
 {
-    public static class ContentResponseHelper
-    {
-        private static readonly ConcurrentDictionary<Type, MethodInfo> _methodCache = new ConcurrentDictionary<Type, MethodInfo>();
+	public static class ContentResponseHelper
+	{
+		private static readonly ConcurrentDictionary<Type, MethodInfo> _methodCache = new ConcurrentDictionary<Type, MethodInfo>();
 
-        public static void SetContent<T>(this ContentResponse<T> response, object? content, bool? success, HttpStatusCode? code, params Error[] errors)
-        {
-            if (content is T typedContent)
-            {
-                response.Success = success;
-                response.Content = typedContent;
-            }
-            else if (content == null)
-            {
-                response.Success = success;
-                response.Content = default;
-            }
-            else
-            {
-                // Try to convert the content
-                try
-                {
-                    var targetType = typeof(T);
-                    var underlyingType = Nullable.GetUnderlyingType(targetType);
-                    
-                    if (underlyingType != null)
-                    {
-                        response.Content = (T)Convert.ChangeType(content, underlyingType);
-                        response.Success = success;
-                    }
-                    else
-                    {
-                        response.Content = (T)Convert.ChangeType(content, targetType);
-                        response.Success = success;
-                    }
-                }
-                catch
-                {
-                    response.Success = false;
-                    response.Content = default;
-                }
-            }
+		public static void SetContent<T>(this ContentResponse<T> response, object? content, bool? success, HttpStatusCode? code, params Error[] errors)
+		{
+			if (content is T typedContent)
+			{
+				response.Success = success;
+				response.Content = typedContent;
+			}
+			else if (content == null)
+			{
+				response.Success = success;
+				response.Content = default;
+			}
+			else
+			{
+				// Try to convert the content
+				try
+				{
+					var targetType = typeof(T);
+					var underlyingType = Nullable.GetUnderlyingType(targetType);
 
-            if (code.HasValue)
-            {
-                response.Status = code.Value;
-            }
+					if (underlyingType != null)
+					{
+						response.Content = (T)Convert.ChangeType(content, underlyingType);
+						response.Success = success;
+					}
+					else
+					{
+						response.Content = (T)Convert.ChangeType(content, targetType);
+						response.Success = success;
+					}
+				}
+				catch
+				{
+					response.Success = false;
+					response.Content = default;
+				}
+			}
 
-            if (errors != null && errors.Length > 0)
-            {
-                response.Errors = errors;
-            }
-        }
+			if (code.HasValue)
+			{
+				response.Status = code.Value;
+			}
 
-        public static void SetContent(this ContentResponse response, object? content, bool? success, HttpStatusCode? code, params Error[] errors)
-        {
-            var contentType = response.ContentType;
-            
-            try
-            {
-                // Get or create cached method
-                var method = _methodCache.GetOrAdd(contentType, type =>
-                {
-                    var genericMethod = typeof(ContentResponseHelper)
-                        .GetMethods(BindingFlags.Public | BindingFlags.Static)
-                        .FirstOrDefault(m => 
-                            m.Name == nameof(SetContent) && 
-                            m.IsGenericMethod && 
-                            m.GetParameters().Length == 5 &&
-                            m.GetParameters()[0].ParameterType.IsGenericType);
+			if (errors != null && errors.Length > 0)
+			{
+				response.Errors = errors;
+			}
+		}
 
-                    return genericMethod?.MakeGenericMethod(type)!;
-                });
+		public static void SetContent(this ContentResponse response, object? content, bool? success, HttpStatusCode? code, params Error[] errors)
+		{
+			var contentType = response.ContentType;
 
-                if (method == null)
-                {
-                    response.Success = false;
-                    return;
-                }
+			try
+			{
+				// Get or create cached method
+				var method = _methodCache.GetOrAdd(contentType, type =>
+				{
+					var genericMethod = typeof(ContentResponseHelper)
+						.GetMethods(BindingFlags.Public | BindingFlags.Static)
+						.FirstOrDefault(m =>
+							m.Name == nameof(SetContent) &&
+							m.IsGenericMethod &&
+							m.GetParameters().Length == 5 &&
+							m.GetParameters()[0].ParameterType.IsGenericType);
 
-                method.Invoke(null, new[] { response, content, success, code, errors });
-            }
-            catch
-            {
-                response.Success = false;
-            }
-        }
+					return genericMethod?.MakeGenericMethod(type)!;
+				});
 
-        public static void SetSuccess<T>(this ContentResponse<T> response, object? content)
-        {
-            response.SetContent(content, true, HttpStatusCode.OK);
-        }
+				if (method == null)
+				{
+					response.Success = false;
+					return;
+				}
 
-        public static void SetSuccess(this ContentResponse response, object? content)
-        {
-            response.SetContent(content, true, HttpStatusCode.OK);
-        }
+				method.Invoke(null, new[] { response, content, success, code, errors });
+			}
+			catch
+			{
+				response.Success = false;
+			}
+		}
 
-        public static void SetErrors<T>(this ContentResponse<T> response, HttpStatusCode code, params Error[] errors)
-        {
-            response.SetContent(default, false, code, errors);
-        }
+		public static void SetSuccess<T>(this ContentResponse<T> response, object? content)
+		{
+			response.SetContent(content, true, HttpStatusCode.OK);
+		}
 
-        public static void SetErrors(this ContentResponse response, HttpStatusCode code, params Error[] errors)
-        {
-            response.SetContent(null, false, code, errors);
-        }
-    }
+		public static void SetSuccess(this ContentResponse response, object? content)
+		{
+			response.SetContent(content, true, HttpStatusCode.OK);
+		}
+
+		public static void SetErrors<T>(this ContentResponse<T> response, HttpStatusCode code, params Error[] errors)
+		{
+			response.SetContent(default, false, code, errors);
+		}
+
+		public static void SetErrors(this ContentResponse response, HttpStatusCode code, params Error[] errors)
+		{
+			response.SetContent(null, false, code, errors);
+		}
+	}
 }
